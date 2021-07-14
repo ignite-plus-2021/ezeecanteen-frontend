71bd5231f3daf93db6c52fb6e0e93e7095a5c67e
Shivanyapm <73844332+Shivanyapm@users.noreply.github.com>
2021-07-14 20:49:44 +0530

    Add files via upload

---
+++ src/Data/BreakfastMenu.js
@@ -0,0 +1,9 @@
+const BreakfastMenu = [
+    { id: 1, image: "../assets/Images/ButterMasalaDosa.jpg", foodname: "vu", price: "320", votes: "258" }
+    // { id: 2,image:,foodname,price,add,rating,votes},
+    // { id: 3,image:,foodname,price,add,rating,votes},
+    // { id: 4,image:,foodname,price,add,rating,votes},
+    // { id: 5,image:,foodname,price,add,rating,votes},
+
+];
+export default BreakfastMenu;
